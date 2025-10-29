c47cdfe0cfe510acace77faaa9b28c5a184ebbea
Phoenix Sheppard <phoenixsheppard28@gmail.com>
2025-10-29 18:07:48 -0400

    cmake

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+#include <unistd.h>
+#include <sstream>
+#include <cerrno>
+#include <cstring>
+#include <string>
+#include <limits.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+
+int main(){
+
+    fork();
+}
