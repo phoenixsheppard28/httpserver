37dc7ea197988293a73949617e55dbb8d0443488
Phoenix Sheppard <phoenixsheppard28@gmail.com>
2025-10-29 22:52:44 -0400

    playing w threads

--- main.cpp
+++ main.cpp
@@ -9,9 +9,23 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <thread>
+#include "helpers.h"
+#include <time.h>
+
+using namespace std;
 
 
 int main(){
-
-    fork();
+    std::thread t1(thread_func,20);
+    std::thread t2(thread_func,10);
+    t1.join();
+    t2.join();
 }
+
+
+void thread_func(int arg1){
+    sleep(1);
+    cout << "Hello! " << arg1 << "\n";
+}
+
