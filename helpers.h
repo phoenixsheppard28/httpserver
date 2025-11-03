a906dede88a01249312ab15698759e8814fcce00
Phoenix Sheppard <phoenixsheppard28@gmail.com>
2025-11-03 00:11:37 -0500

    socket

--- helpers.h
+++ helpers.h
@@ -1,3 +1,5 @@
 #pragma once
 
 void thread_func(int x);
+
+void cleanup(void);
--- main.cpp
+++ main.cpp
@@ -1,29 +1,45 @@
-#include <iostream>
-#include <cstring>        // memset, etc.
-#include <iostream>       // for printing/logging
-#include <cstring>
-#include <string>
-#include <thread>
-#include <sys/socket.h>   // main socket functions
-#include <netinet/in.h>   // sockaddr_in structure (for IP addresses)
-#include <arpa/inet.h>    // inet_pton / inet_ntop helpers
-#include <unistd.h>       // close() function
-#include <time.h>
 #include "helpers.h"
+#include <arpa/inet.h> // inet_pton / inet_ntop helpers
+#include <cstring>     // memset, etc.
+#include <cstring>
+#include <iostream>
+#include <iostream>     // for printing/logging
+#include <netinet/in.h> // sockaddr_in structure (for IP addresses)
+#include <stdlib.h>
+#include <string>
+#include <sys/socket.h> // main socket functions
+#include <thread>
+#include <time.h>
+#include <unistd.h> // close() function
 
 using namespace std;
 
+int sock;
 
-int main(){
-    
-    std::thread t1(thread_func,20);
-    std::thread t2(thread_func,10);
-    t1.join();
-    t2.join();
+void thread_func(int arg1) { cout << "Hello! " << arg1 << "\n"; }
+
+// std::thread t1(thread_func,20);
+// std::thread t2(thread_func,10);
+// t1.join();
+// t2.join();
+
+void cleanup(void) {
+  cout << "Cleaning up socket " << sock;
+
+  shutdown(sock, SHUT_RDWR);
+  close(sock);
 }
 
-
-void thread_func(int arg1){
-    cout << "Hello! " << arg1 << "\n";
+void handle_sigint(int signum) {
+  cout << "\nCaught SIGINT (" << signum << ")" << endl;
+  exit(0); // ensure atexit handlers and normal shutdown run
 }
 
+int main() {
+  sock = socket(AF_INET, SOCK_STREAM, 0);
+  atexit(cleanup);
+  signal(SIGINT, handle_sigint);
+
+  while (true) {
+  }
+}
