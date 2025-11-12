f1e24b666c1a8c8c015e5dcbb2e735b66e4c9f51
Phoenix Sheppard <phoenixsheppard28@gmail.com>
2025-11-12 12:22:42 -0500

    hi

--- main.cpp
+++ main.cpp
@@ -8,15 +8,14 @@
 #include <stdlib.h>
 #include <string>
 #include <sys/socket.h> // main socket functions
+#include <sys/types.h>
 #include <thread>
 #include <time.h>
 #include <unistd.h> // close() function
 
-using namespace std;
+int sock = -1;
 
-int sock;
-
-void thread_func(int arg1) { cout << "Hello! " << arg1 << "\n"; }
+void thread_func(int arg1) { std::cout << "Hello! " << arg1 << "\n"; }
 
 // std::thread t1(thread_func,20);
 // std::thread t2(thread_func,10);
@@ -24,22 +23,52 @@ void thread_func(int arg1) { cout << "Hello! " << arg1 << "\n"; }
 // t2.join();
 
 void cleanup(void) {
-  cout << "Cleaning up socket " << sock;
+  std::string msg = sock != -1 ? "Cleaning up socket " + std::to_string(sock)
+                               : "No socket ever connected";
+  std::cout << msg << "\n";
 
   shutdown(sock, SHUT_RDWR);
   close(sock);
 }
 
 void handle_sigint(int signum) {
-  cout << "\nCaught SIGINT (" << signum << ")" << endl;
+  std::cout << "\nCaught SIGINT (" << signum << ")" << std::endl;
   exit(0); // ensure atexit handlers and normal shutdown run
 }
 
 int main() {
-  sock = socket(AF_INET, SOCK_STREAM, 0);
   atexit(cleanup);
   signal(SIGINT, handle_sigint);
+  sock = socket(AF_INET, SOCK_STREAM, 0);
+  if (sock == -1) {
+    std::cout << "Error opening socket" << std::endl;
+    exit(1);
+  }
+
+  sockaddr_in serverAddress;
+  memset(&serverAddress, 0, sizeof(serverAddress));
+  serverAddress.sin_family = AF_INET;
+  serverAddress.sin_port = htons(8000);
+  serverAddress.sin_addr.s_addr = INADDR_ANY; // Bind to any available interface
+
+  socklen_t addrLen = sizeof(serverAddress);
+
+  if (bind(sock, (struct sockaddr *)&serverAddress, addrLen) == -1) {
+    std::cout << "Error binding socket" << std::endl;
+    exit(1);
+  }
+
+  if (listen(sock, 8000) == -1) {
+    std::cout << "Error listening to port" << std::endl;
+    return 1;
+  }
 
   while (true) {
+    int client_sock = accept(sock, (struct sockaddr *)&serverAddress, &addrLen);
+    if (client_sock == -1) {
+      std::cout << "Error accepting connection" << std::endl;
+      return 1;
+    }
+    std::cout << "HI";
   }
 }
